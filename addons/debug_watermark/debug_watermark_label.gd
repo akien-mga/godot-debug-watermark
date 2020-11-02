4b5bc7fd2bc845394d57ad6e37fce4b1b55b2825
Rémi Verschelde <rverschelde@gmail.com>
2020-11-02 13:31:15 +0100

    Fix position of custom tooltip
    This requires working around two engine issues:
    
    - RichTextLabel doesn't properly set its minimum size:
      https://github.com/godotengine/godot/issues/18260
    
    - Custom tooltip placement fails if the custom tooltip is not visible
      beforehand. It's not a bug per se, but documentation will need to be
      made clearer about this requirement:
      https://github.com/godotengine/godot/issues/39677

--- addons/debug_watermark/debug_watermark_label.gd
+++ addons/debug_watermark/debug_watermark_label.gd
@@ -7,7 +7,12 @@ func _ready():
 
 func _make_custom_tooltip(for_text):
 	var tooltip = $Tooltip.duplicate()
-	var rtl = tooltip.get_node("MarginContainer/RichTextLabel")
+	# Must be made visible beforehand, otherwise Viewport size calculations
+	# would be incorrect (see https://github.com/godotengine/godot/issues/39677).
+	tooltip.visible = true
+	var rtl = tooltip.get_node("PanelContainer/MarginContainer/RichTextLabel")
 	rtl.bbcode_text = for_text
+	# Work around https://github.com/godotengine/godot/issues/18260.
+	rtl.rect_min_size = rtl.rect_size
 	return tooltip
 
